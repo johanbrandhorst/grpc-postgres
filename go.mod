3be72b22ef2abaaa676c9addd7ab23fb8cef7b0c
Johan Brandhorst <johan.brandhorst@gmail.com>
2019-05-13 23:17:01 +0100

    main: add grpcui handler

--- go.mod
+++ go.mod
@@ -5,19 +5,24 @@ go 1.12
 require (
 	github.com/Masterminds/squirrel v1.1.0
 	github.com/cenkalti/backoff v2.1.1+incompatible // indirect
+	github.com/cockroachdb/cmux v0.0.0-20170110192607-30d10be49292
 	github.com/containerd/continuity v0.0.0-20181203112020-004b46473808 // indirect
 	github.com/docker/docker v0.7.3-0.20190212235812-0111ee70874a // indirect
+	github.com/fullstorydev/grpcui v0.1.0
+	github.com/fullstorydev/grpcurl v1.1.0
 	github.com/golang-migrate/migrate/v4 v4.2.5
 	github.com/golang/mock v1.2.0
 	github.com/golang/protobuf v1.3.1
 	github.com/gorilla/mux v1.7.0 // indirect
 	github.com/gotestyourself/gotestyourself v2.2.0+incompatible // indirect
 	github.com/jackc/pgx v3.3.0+incompatible
+	github.com/jhump/protoreflect v1.1.0
 	github.com/jteeuwen/go-bindata v3.0.7+incompatible
 	github.com/morikuni/aec v0.0.0-20170113033406-39771216ff4c // indirect
 	github.com/opencontainers/runc v0.1.1 // indirect
 	github.com/ory/dockertest v3.3.4+incompatible
 	github.com/sirupsen/logrus v1.4.1
+	github.com/soheilhy/cmux v0.1.4
 	golang.org/x/net v0.0.0-20190311183353-d8887717615a // indirect
 	google.golang.org/genproto v0.0.0-20190307195333-5fe7a883aa19 // indirect
 	google.golang.org/grpc v1.19.1
--- main.go
+++ main.go
@@ -1,18 +1,26 @@
 package main
 
 import (
+	"context"
 	"crypto/tls"
+	"crypto/x509"
 	"errors"
 	"flag"
 	"fmt"
 	"net"
+	"net/http"
 	"net/url"
 	"time"
 
+	"github.com/fullstorydev/grpcui/standalone"
+	"github.com/fullstorydev/grpcurl"
+	"github.com/jhump/protoreflect/desc"
+	"github.com/jhump/protoreflect/grpcreflect"
 	"github.com/sirupsen/logrus"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/reflection"
+	reflectpb "google.golang.org/grpc/reflection/grpc_reflection_v1alpha"
 
 	pbUsers "github.com/johanbrandhorst/grpc-postgres/proto"
 	"github.com/johanbrandhorst/grpc-postgres/users"
@@ -41,10 +49,11 @@ func (p pgURL) String() string {
 }
 
 var (
-	port = flag.Int("port", 10000, "The server port")
-	cert = flag.String("cert", "./insecure/cert.pem", "The path to the server certificate file in PEM format")
-	key  = flag.String("key", "./insecure/key.pem", "The path to the server private key in PEM format")
-	u    pgURL
+	port     = flag.Int("port", 10000, "The gRPC server port")
+	httpPort = flag.Int("http_port", 11000, "The HTTP UI server port")
+	cert     = flag.String("cert", "./insecure/cert.pem", "The path to the server certificate file in PEM format")
+	key      = flag.String("key", "./insecure/key.pem", "The path to the server private key in PEM format")
+	u        pgURL
 )
 
 func main() {
@@ -61,18 +70,18 @@ func main() {
 		log.Fatal("Flag postgres-url is required")
 	}
 
-	cert, err := tls.LoadX509KeyPair(*cert, *key)
+	tlsCert, err := tls.LoadX509KeyPair(*cert, *key)
 	if err != nil {
 		log.WithError(err).Fatal("Failed to parse certificate and key")
 	}
+	tlsCert.Leaf, _ = x509.ParseCertificate(tlsCert.Certificate[0])
 
-	addr := fmt.Sprintf(":%d", *port)
-	lis, err := net.Listen("tcp", addr)
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
 	if err != nil {
 		log.WithError(err).Fatal("Failed to listen")
 	}
 	s := grpc.NewServer(
-		grpc.Creds(credentials.NewServerTLSFromCert(&cert)),
+		grpc.Creds(credentials.NewServerTLSFromCert(&tlsCert)),
 	)
 	reflection.Register(s)
 
@@ -83,6 +92,78 @@ func main() {
 	pbUsers.RegisterUserServiceServer(s, dir)
 
 	// Serve gRPC Server
-	log.Info("Serving gRPC on https://", addr)
-	log.Fatal(s.Serve(lis))
+	go func() {
+		log.Info("Serving gRPC on ", lis.Addr().String())
+		log.Fatal(s.Serve(lis))
+	}()
+
+	cp := x509.NewCertPool()
+	cp.AddCert(tlsCert.Leaf)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	sAddr := fmt.Sprintf("dns:///localhost:%d", *port)
+	cc, err := grpc.DialContext(
+		ctx,
+		sAddr,
+		grpc.WithBlock(),
+		grpc.WithTransportCredentials(credentials.NewClientTLSFromCert(cp, "")),
+	)
+	if err != nil {
+		log.WithError(err).Fatal("Failed to dial local server")
+	}
+	defer cc.Close()
+
+	m, f, err := getMethodsAndFiles(ctx, cc)
+	if err != nil {
+		log.WithError(err).Fatal("Failed to get server properties")
+	}
+
+	httpS := &http.Server{
+		Addr:    fmt.Sprintf(":%d", *httpPort),
+		Handler: standalone.Handler(cc, sAddr, m, f),
+	}
+
+	// Serve HTTP Server
+	log.Info("Serving HTTP UI on http://localhost", httpS.Addr)
+	log.Fatal(httpS.ListenAndServe())
+}
+
+func getMethodsAndFiles(ctx context.Context, cc *grpc.ClientConn) ([]*desc.MethodDescriptor, []*desc.FileDescriptor, error) {
+	c := grpcreflect.NewClient(ctx, reflectpb.NewServerReflectionClient(cc))
+	source := grpcurl.DescriptorSourceFromServer(ctx, c)
+
+	allServices, err := source.ListServices()
+	if err != nil {
+		return nil, nil, err
+	}
+
+	var m []*desc.MethodDescriptor
+	for _, svc := range allServices {
+		d, err := source.FindSymbol(svc)
+		if err != nil {
+			return nil, nil, err
+		}
+		sd, ok := d.(*desc.ServiceDescriptor)
+		if !ok {
+			return nil, nil, fmt.Errorf("%s should be a service descriptor but instead is a %T", d.GetFullyQualifiedName(), d)
+		}
+		if sd.GetFullyQualifiedName() == "grpc.reflection.v1alpha.ServerReflection" {
+			continue // skip reflection service
+		}
+		for _, md := range sd.GetMethods() {
+			m = append(m, md)
+		}
+	}
+
+	f, err := grpcurl.GetAllFiles(source)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	// Release resources
+	c.Reset()
+
+	return m, f, nil
 }
